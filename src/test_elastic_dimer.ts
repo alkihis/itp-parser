77f7ab7402a15ed255bf570991e361b2001858ed
Cecile Hilpert <cecile.hilpert@gmail.com>
2021-11-03 15:00:41 +0100

    subfield without header

--- src/test_elastic_dimer.ts
+++ src/test_elastic_dimer.ts
@@ -6,20 +6,22 @@ const FILES = path.resolve(__dirname, '../itp_test/dimer_elastic');
 
 (async() => {
     const top = await TopFile.read(FILES + "/1a2w.top", [FILES + "/1a2w_A.itp", FILES + "/1a2w_B.itp"])
-    const file = top.molecules[0].itp
 
-    const rubberBands = file.getSubfield("bonds", "Rubber band")
+    for (const mol of top.molecules){
+        const file = mol.itp; 
+        const rubberBands = file.getSubfield("bonds", "Rubber band")
 
-    const itp = new ItpFile();
+        const itp = new ItpFile();
+        
+        itp.appendField("bonds", rubberBands)
+        fs.writeFileSync(FILES + "/" + mol.type + "_without_rubber.itp", itp.toString())
     
-    itp.appendField("bonds", rubberBands)
-    fs.writeFileSync(FILES + "/rubber.itp", itp.toString())
-
-    
-    file.removeSubfield("bonds", "Rubber band"); 
-    file.appendInclude("rubber.itp", "bonds"); 
-    
-    fs.writeFileSync(FILES + "/without_rubber.itp", file.toString())
+        file.removeSubfield("bonds", "Rubber band"); 
+        file.appendInclude(mol.type + "_rubber.itp", "bonds"); 
+        
+        fs.writeFileSync(FILES + "/" + mol.type + "_rubber.itp", file.toString())
+    }
+   
 
 
 })();
